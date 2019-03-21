440d8568299c84434f594d62ee3aa16f88df09dc
Luis Márquez <luis.marquez@proyectosfgk.org>
2019-03-21 09:01:33 -0600

    añadiendo proyecto

---
+++ js/print.js
@@ -0,0 +1,12 @@
+var base_url = 'http://demo.web-informatica.info/Miralaw/';
+
+function printInvoices(){
+	var date1 = $('#date1').val();
+	var date2 = $('#date2').val();
+
+	if (date1=='' || date2=='') {
+		alert('seleccione una fecha correcta');
+	}
+
+	window.location.replace(base_url+'Billing/invoicesToPrint?fecha1='+date1+'&fecha2='+date2);
+}
